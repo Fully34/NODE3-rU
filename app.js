20a1b6982085dce13f94c81b5c37e67e13547b39
Fully34 <cfullinw34@gmail.com>
2015-07-21 15:34:38 -0600

    finished

--- app.js
+++ app.js
@@ -2,13 +2,28 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var indexController = require('./controllers/index.js');
 
+
+//============================== set up jade + views ==============================//
+        
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
+
+//============================== .use stuff ==============================//
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
+//============================== get handlers ==============================//
+        
 app.get('/', indexController.index);
+
+// what to do on formsubmit
+app.post('/formsubmit', indexController.formSubmit);
+
+// redirect will trigger a request for '/success'
+    // Just want to send the user a message
+app.get('/success', indexController.successPage);
 
 var server = app.listen(7449, function() {
 	console.log('Express server listening on port ' + server.address().port);
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,21 @@
 var indexController = {
 	index: function(req, res) {
 		res.render('index');
-	}
+	},
+
+    formSubmit : function(req, res) {
+
+        // console.log(req.body); //> this is an object in the form of { email : 'email@email.com'}
+
+        // redirect the user when they submit the form
+        res.redirect('/success');
+    },
+
+    successPage : function(req, res) {
+
+        // console.log(req.body); //> there is no body property here
+        res.send("GREAT JOB, YOU USED A FORM!  You're the coolest person ever. /s")
+    }
 };
 
 module.exports = indexController;
